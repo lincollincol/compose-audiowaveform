d53175a07585ba66ff56defdb88be4a17ca92b19
Andrew Linc <andriy.serb1@gmail.com>
2023-02-12 14:21:39 +0200

    Normalize data. Added fillToSize() function to handle case when spikes > ampls

--- app/src/main/java/com/linc/audiowaveform/sample/ui/screen/waveform/AudioWaveformViewModel.kt
+++ app/src/main/java/com/linc/audiowaveform/sample/ui/screen/waveform/AudioWaveformViewModel.kt
@@ -65,12 +65,7 @@ class AudioWaveformViewModel @Inject constructor(
 
     private suspend fun loadAudioAmplitudes(localAudio: LocalAudio) {
         try {
-//            val amplitudes = audioRepository.loadAudioAmplitudes(localAudio)
-            val amplitudes = buildList {
-                repeat(100) {
-                    add(it)
-                }
-            }
+            val amplitudes = audioRepository.loadAudioAmplitudes(localAudio)
             uiState = uiState.copy(amplitudes = amplitudes)
         } catch (e: Exception) {
             e.printStackTrace()
